ddc16642beb42144a97fda7517d7ba9a87a054e3
ugurlugelen <augurlugelen@gmail.com>
2021-07-07 01:14:25 +0300

    calısma

--- src/test/java/pages/US_014_Negatif_RoomReservasyon.java
+++ src/test/java/pages/US_014_Negatif_RoomReservasyon.java
@@ -1,6 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_014_Negatif_RoomReservasyon {
+
+    public US_014_Negatif_RoomReservasyon(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy
+    public WebElement hotelListManagementButton;
 
 
 
