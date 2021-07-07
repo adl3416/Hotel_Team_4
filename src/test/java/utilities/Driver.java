e1e70b7c37f1e7da9c12f322d1517089e663c7a2
ugurlugelen <augurlugelen@gmail.com>
2021-07-07 23:56:13 +0300

    config update

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,10 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -33,7 +30,7 @@ public class Driver {
             }
         }
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         return driver;
     }
