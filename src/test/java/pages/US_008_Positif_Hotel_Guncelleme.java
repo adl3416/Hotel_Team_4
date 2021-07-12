fc4b10eb251f10535321197615db84f58277cafe
adl <makara3416@gmail.com>
2021-07-12 23:34:56 +0200

    dene

---
+++ src/test/java/pages/US_008_Positif_Hotel_Guncelleme.java
@@ -0,0 +1,71 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_008_Positif_Hotel_Guncelleme {
+
+
+
+
+        public US_008_Positif_Hotel_Guncelleme() {
+
+            PageFactory.initElements(Driver.getDriver(),this);
+        }
+        @FindBy(partialLinkText ="Log")
+        public WebElement LoginButonu;
+
+        @FindBy(css ="#UserName")
+        public WebElement usurname;
+
+        @FindBy(xpath = "//input[@id='Password']")
+        public WebElement password;
+
+        @FindBy (xpath = "//input[@id=\"btnSubmit\"]")
+        public WebElement sonLoginButonu;
+
+        @FindBy(xpath = "(//span[@class='title'])[3]")
+        public WebElement hotelManagment;
+
+        @FindBy(linkText="Hotel List")
+        public WebElement HotelListsekmesi;
+
+        @FindBy(xpath = "//div[@class='caption']")
+        public  WebElement HotelListYazisi;
+
+        @FindBy(xpath = " //a[@class='btn btn-xs default']")
+        public  WebElement DetailsSekmesi;
+
+        @FindBy(xpath = "(//div[@class='caption'])[1]")
+        public WebElement DetailsYazisi;
+
+        @FindBy(xpath ="// input[@id='Phone']")
+        public WebElement PhoneSekmesi;
+
+        @FindBy(xpath = "// button[@class='btn green'][1]")
+        public WebElement SaveSekmesi;
+
+        @FindBy(xpath = "// div[@class='bootbox-body']")
+        public WebElement SaveOlduYazisi;
+
+        @FindBy(xpath ="//input[@title='Email']")
+        public WebElement EmailSekmesi;
+
+        @FindBy(xpath = "//select[@id='IDGroup']")
+        public WebElement GroupSekmesi;
+
+        @FindBy(xpath = "//option[@value='1']")
+        public WebElement Group1;
+
+        @FindBy(xpath = "(// div[@class='portlet-title'])[4]")
+        public WebElement DeleteSekmesi;
+
+        @FindBy(xpath = "//button[@class='btn btn-primary']")
+        public WebElement DevammiText;
+
+
+
+
+}
---
+++ src/test/java/tests/US_008.java
@@ -0,0 +1,97 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.US_008_Positif_Hotel_Guncelleme;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class US_008 {
+    US_008_Positif_Hotel_Guncelleme pagesSayfam = new US_008_Positif_Hotel_Guncelleme();
+    @BeforeClass
+    public void setup() {
+        Driver.getDriver().get(ConfigReader.getProperty("kr_url"));
+        pagesSayfam.LoginButonu.click();
+        pagesSayfam.usurname.sendKeys(ConfigReader.getProperty("kr_valid_username"));
+        pagesSayfam.password.sendKeys(ConfigReader.getProperty("kr_valid_password"));
+        pagesSayfam.sonLoginButonu.click();
+
+
+
+    }
+    @Test
+    public void us008_TestCase01 () {
+        pagesSayfam.hotelManagment.click();
+        pagesSayfam.HotelListsekmesi.click();
+        Assert.assertTrue(pagesSayfam.HotelListYazisi.isDisplayed());
+    }
+    @Test
+    public void us008_TestCase02 () throws InterruptedException {
+        pagesSayfam.hotelManagment.click();
+        pagesSayfam.HotelListsekmesi.click();
+        pagesSayfam.DetailsSekmesi.click();
+        Thread.sleep(9000);
+        Assert.assertTrue(pagesSayfam.DetailsYazisi.isDisplayed());
+
+
+
+    }
+    @Test
+    public void us008_TestCase03 () throws InterruptedException {
+        pagesSayfam.hotelManagment.click();
+        pagesSayfam.HotelListsekmesi.click();
+        pagesSayfam.DetailsSekmesi.click();
+        pagesSayfam.PhoneSekmesi.click();
+        pagesSayfam.PhoneSekmesi.clear();
+        pagesSayfam.PhoneSekmesi.sendKeys(ConfigReader.getProperty("US_008_guncelhotel_phone"));
+        pagesSayfam.SaveSekmesi.click();
+        Thread.sleep(9000);
+        Assert.assertTrue(pagesSayfam.SaveOlduYazisi.isDisplayed());
+
+    }
+
+
+    @Test
+    public void us008_TestCase04 () throws InterruptedException {
+        pagesSayfam.hotelManagment.click();
+        pagesSayfam.HotelListsekmesi.click();
+        pagesSayfam.DetailsSekmesi.click();
+        pagesSayfam.EmailSekmesi.click();
+        pagesSayfam.EmailSekmesi.clear();
+        pagesSayfam.EmailSekmesi.sendKeys(ConfigReader.getProperty("US_008_guncelhotel_email"));
+        pagesSayfam.SaveSekmesi.click();
+        Thread.sleep(9000);
+        Assert.assertTrue(pagesSayfam.SaveOlduYazisi.isDisplayed());
+    }
+
+
+    @Test
+    public void us008_TestCase05 () throws InterruptedException {
+        pagesSayfam.hotelManagment.click();
+        pagesSayfam.HotelListsekmesi.click();
+        pagesSayfam.DetailsSekmesi.click();
+        pagesSayfam.GroupSekmesi.click();
+        pagesSayfam.Group1.click();
+        pagesSayfam.SaveSekmesi.click();
+        Thread.sleep(9000);
+        Assert.assertTrue(pagesSayfam.SaveOlduYazisi.isDisplayed());
+    }
+
+    @Test
+    public void us008_TestCase06 () throws InterruptedException {
+        pagesSayfam.hotelManagment.click();
+        pagesSayfam.HotelListsekmesi.click();
+        pagesSayfam.DetailsSekmesi.click();
+        pagesSayfam.DeleteSekmesi.click();
+        pagesSayfam.DevammiText.click();
+
+    }
+
+
+
+}
